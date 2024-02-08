6a79b16fbd374f9cda9773cac27f42e42bd06c86
anasadovoi <44027515+anasadovoi@users.noreply.github.com>
2024-02-08 08:21:31 +0200

    example for video 9_17 (while)

---
+++ draw-line.py
@@ -0,0 +1,12 @@
+from os import system
+#draw a line using whilel of a  length given from kb
+
+#length: 3
+system("cls")
+length = int(input('length:  '))
+
+n=0
+
+while n < length:
+    print("-", end ="")
+    n += 1
